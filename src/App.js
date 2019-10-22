c21966ac8d05d3d9d971863fed42929238e265bd
Ujjawal Dixit <ujjawaldixit099@gmail.com>
2019-10-22 17:02:13 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          I am Ujjawal Dixit
+        </p>
+        <p className="Small-font">
+          Software Engineer @ GOJEK
+        </p>
+      </header>
+    </div>
+  );
+}
+export default App;
