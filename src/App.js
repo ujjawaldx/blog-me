f6ace705d393c16bedf5ca985beec81ad77f32f1
Ujjawal Dixit <ujjawaldixit099@gmail.com>
2020-06-06 20:57:53 +0530

    Add basic layout design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './App.css';
+import medium_icon from './medium-icon.png'
+import twitter_icon from './twitter-icon.png'
+import linkedin_icon from './linkedin-icon.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          I am Ujjawal Dixit
-        </p>
-        <p className="Small-font">
-          Software Engineer @ GOJEK
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Ujjawal Dixit</p>
+                <p className="Small-font">Software Engineer @ GOJEK</p>
+                <p className="Social-Links">
+                    <a href="https://twitter.com/DixitUjjawal">
+                        <img className="twitter-image" src={twitter_icon} alt={"twitter-icon"}/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/ujjawaldx/">
+                        <img className="linkedin-image" src={linkedin_icon} alt={"linkedin-icon"}/>
+                    </a>
+                    <a href="https://medium.com/@ujjawaldixit099">
+                        <img className="medium-image" src={medium_icon} alt={"facebook-icon"}/>
+                    </a>
+                </p>
+            </header>
+        </div>
+    );
 }
+
 export default App;
