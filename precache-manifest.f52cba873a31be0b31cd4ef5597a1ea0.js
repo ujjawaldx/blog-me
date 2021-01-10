5899c38d7039342a356c7f4edf6d6858d04668bd
Ujjawal Dixit <ujjawaldixit099@gmail.com>
2021-01-10 20:52:34 +0530

    Updates

---
+++ precache-manifest.f52cba873a31be0b31cd4ef5597a1ea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efda24f3b9767b6814f4f1c1874685bf",
+    "url": "/blog-me/index.html"
+  },
+  {
+    "revision": "f965cc9e1b94230f5b08",
+    "url": "/blog-me/static/css/main.18179b58.chunk.css"
+  },
+  {
+    "revision": "b8b0461e19998ac65c23",
+    "url": "/blog-me/static/js/2.6d12fa3c.chunk.js"
+  },
+  {
+    "revision": "f965cc9e1b94230f5b08",
+    "url": "/blog-me/static/js/main.4d277fbe.chunk.js"
+  },
+  {
+    "revision": "4a42d445f5ad33da130d",
+    "url": "/blog-me/static/js/runtime-main.cb4abee9.js"
+  },
+  {
+    "revision": "2754959a9804d9a3a651d73d4f47a204",
+    "url": "/blog-me/static/media/background.2754959a.jpg"
+  },
+  {
+    "revision": "78619b411c20dfe04e09451455f3c321",
+    "url": "/blog-me/static/media/profile_picture.78619b41.png"
+  }
+]);
